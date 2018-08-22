bd99f22ae518fe3e545d00e0d8f2a06fc8e722bf
atom <jintong.liu@outlook.com>
2018-08-22 08:31:50 +0800

    feat: add effect of loading more

--- src/main.js
+++ src/main.js
@@ -8,21 +8,22 @@ Vue.use(VueResource)
 import 'mint-ui/lib/style.css'
 
 import {
-  Button, IndexList, IndexSection, Cell, Search, Header
+  Button, IndexList, IndexSection, Cell, Search, Header, InfiniteScroll, Spinner
 }
 from 'mint-ui'
 
+Vue.use(InfiniteScroll)
 Vue.component(Button.name, Button)
 Vue.component(IndexList.name, IndexList)
 Vue.component(IndexSection.name, IndexSection)
 Vue.component(Cell.name, Cell)
 Vue.component(Search.name, Search)
 Vue.component(Header.name, Header)
+Vue.component(Spinner.name, Spinner)
 
 import SliderMenu from './components/SliderMenu.vue'
 
 Vue.component('slider-menu', SliderMenu)
-
 
 Vue.config.productionTip = false
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <mt-index-list>
+    <mt-index-list
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="loading"
+      infinite-scroll-distance="10"
+    >
       <mt-index-section v-bind:index="section.name" v-for="(section) in sections">
         <router-link v-bind:to="`/games/${game.id}`" v-for="game in section.games">
           <mt-cell v-bind:title="game.name"></mt-cell>
         </router-link>
       </mt-index-section>
     </mt-index-list>
+    <p v-show="loading" class="loading">
+      <mt-spinner type="fading-circle" class="loading-spinner"></mt-spinner>
+      加载中...
+    </p>
   </div>
 </template>
 
@@ -14,6 +22,7 @@
   export default {
     data() {
       return {
+        loading: false,
         sections: [
           {
             name: 'A',
@@ -28,14 +37,69 @@
               {id: 171668, name: '步兵的恐惧'},
               {id: 231554, name: '本草'},
             ]
-          }
-        ]
+          },
+          {
+            name: 'A',
+            games: [
+              {id: 177736, name: '奥丁的盛宴'},
+              {id: 182874, name: '奥地利大酒店'},
+            ]
+          },
+          {
+            name: 'C',
+            games: [
+              {id: 177736, name: '奥丁的盛宴'},
+              {id: 182874, name: '奥地利大酒店'},
+            ]
+          },
+          {
+            name: 'D',
+            games: [
+              {id: 177736, name: '奥丁的盛宴'},
+              {id: 182874, name: '奥地利大酒店'},
+            ]
+          },
+        ],
       }
     },
+    methods: {
+      loadMore() {
+        console.log('loadMore')
+        this.loading = true
+        // TODO
+        setTimeout(() => {
+          this.sections.push({
+            name: 'E',
+            games: [
+              {id: 177736, name: '奥丁的盛宴2'},
+              {id: 182874, name: '奥地利大酒店2'},
+            ]
+          })
+          this.sections.push({
+            name: 'F',
+            games: [
+              {id: 177736, name: '奥丁的盛宴3'},
+              {id: 182874, name: '奥地利大酒店3'},
+            ]
+          })
+          this.loading = false
+        }, 5000)
+      }
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .loading {
+    text-align: center;
+    height: 50px;
+    line-height: 50px;
+  }
 
+  .loading-spinner {
+    display: inline-block;
+    vertical-align: middle;
+    margin-right: 5px;
+  }
 </style>
